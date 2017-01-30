0e91430b4518951c20778bc66713c74c31565eed
Topbit Du <topbit.du@gmail.com>
2017-01-30 19:00:28 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/geo/china/models_rspec.rb
@@ -0,0 +1,2 @@
+require 'rspec/models/unidom/geo/china/region_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Geo::China::Region'
+require 'rspec/models/unidom/geo/china/town_spec'   unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Geo::China::Town'
