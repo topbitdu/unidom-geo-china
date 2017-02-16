d137e8c237ac790fb58d6779b6f66eff077feaf2
Topbit Du <topbit.du@gmail.com>
2017-02-16 10:02:05 +0800

    1, Improve the Town spec for the validations on the #name attribute, & the #numeric_code attribute.

--- lib/rspec/models/unidom/geo/china/town_spec.rb
+++ lib/rspec/models/unidom/geo/china/town_spec.rb
@@ -14,7 +14,44 @@ describe Unidom::Geo::China::Town, type: :model do
       name:         'Some Town'
     }
 
+    name_max_length = described_class.columns_hash['name'].limit
+
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+    it_behaves_like 'validates', model_attributes, :numeric_code,
+      {                             } => 0,
+      { numeric_code: nil           } => 3,
+      { numeric_code: ''            } => 3,
+      { numeric_code: '1'           } => 1,
+      { numeric_code: '1'*8         } => 1,
+      { numeric_code: 11_111_111    } => 1,
+      { numeric_code: "-#{'1'*8}"   } => 1,
+      { numeric_code: -11_111_111   } => 1,
+      { numeric_code: '1'*9         } => 0,
+      { numeric_code: 111_111_111   } => 0,
+      { numeric_code: "-#{'1'*9}"   } => 2,
+      { numeric_code: -111_111_111  } => 2,
+      { numeric_code: '1'*10        } => 1,
+      { numeric_code: 1_111_111_111 } => 1,
+      { numeric_code: "#{'1'*8}A"   } => 1,
+      { numeric_code: '111111.11'   } => 1,
+      { numeric_code: 111111.11     } => 1
+
+    it_behaves_like 'validates', model_attributes, :name,
+      {              } => 0,
+      { name: nil    } => 2,
+      { name: ''     } => 2,
+      { name: '1'    } => 1,
+      { name: '11'   } => 0,
+      { name: 'AA'   } => 0,
+      { name: '111'  } => 0,
+      { name: 'AAA'  } => 0,
+      { name: '1'*(name_max_length-1) } => 0,
+      { name: 'A'*(name_max_length-1) } => 0,
+      { name: '1'*name_max_length     } => 0,
+      { name: 'A'*name_max_length     } => 0,
+      { name: '1'*(name_max_length+1) } => 1,
+      { name: 'A'*(name_max_length+1) } => 1
 
   end
 
