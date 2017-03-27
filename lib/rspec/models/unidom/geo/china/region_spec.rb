89e61b2e57027ff8821f382a0fca22a40a33c4c7
Topbit Du <topbit.du@gmail.com>
2017-03-27 21:15:20 +0800

    1, Improve the Region spec for the As Inferior Region concern.

--- lib/rspec/models/unidom/geo/china/region_spec.rb
+++ lib/rspec/models/unidom/geo/china/region_spec.rb
@@ -1,3 +1,5 @@
+require 'rspec/models/unidom/geo/china/concerns/as_inferior_region_shared_examples'
+
 describe Unidom::Geo::China::Region, type: :model do
 
   before :each do
@@ -16,12 +18,11 @@ describe Unidom::Geo::China::Region, type: :model do
       name:             'Some Region'
     }
 
-    name_max_length = described_class.columns_hash['name'].limit
+    it_behaves_like 'Unidom::Common::Concerns::ModelExtension',       model_attributes.merge(scheme_id: SecureRandom.uuid)
+    it_behaves_like 'Unidom::Geo::Concerns::AsRegion',                model_attributes.merge(scheme_id: SecureRandom.uuid)
+    it_behaves_like 'Unidom::Geo::China::Concerns::AsInferiorRegion', model_attributes.merge(scheme_id: SecureRandom.uuid)
 
-    it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
-    it_behaves_like 'Unidom::Geo::Concerns::AsRegion',          model_attributes
-
-    it_behaves_like 'validates', model_attributes, :numeric_code,
+    it_behaves_like 'validates', model_attributes.merge(scheme_id: SecureRandom.uuid), :numeric_code,
       {                         } => 0,
       { numeric_code: nil       } => 3,
       { numeric_code: ''        } => 3,
@@ -41,9 +42,9 @@ describe Unidom::Geo::China::Region, type: :model do
       { numeric_code: 1111111   } => 1,
       { numeric_code: '11111A'  } => 1
 
-    it_behaves_like 'validates text', model_attributes, :alphabetic_code,
+    it_behaves_like 'validates text', model_attributes.merge(scheme_id: SecureRandom.uuid), :alphabetic_code,
       length: 2..described_class.columns_hash['alphabetic_code'].limit
-    it_behaves_like 'validates text', model_attributes, :name,
+    it_behaves_like 'validates text', model_attributes.merge(scheme_id: SecureRandom.uuid), :name,
       length: 1..described_class.columns_hash['name'].limit
 
     town_1_attributes = {
@@ -56,22 +57,22 @@ describe Unidom::Geo::China::Region, type: :model do
       name:         'Some Town #2'
     }
 
-    it_behaves_like 'has_many', model_attributes, :towns, Unidom::Geo::China::Town, [ town_1_attributes, town_2_attributes ]
+    it_behaves_like 'has_many', model_attributes.merge(scheme_id: SecureRandom.uuid), :towns, Unidom::Geo::China::Town, [ town_1_attributes, town_2_attributes ]
 
     it_behaves_like 'scope', :name_is, [
-      { attributes_collection: [ model_attributes                               ], count_diff: 1, args: [ model_attributes[:name] ] },
-      { attributes_collection: [ model_attributes                               ], count_diff: 0, args: [ 'Another Region'        ] },
-      { attributes_collection: [ model_attributes.merge(name: 'Another Region') ], count_diff: 0, args: [ model_attributes[:name] ] },
-      { attributes_collection: [ model_attributes.merge(name: 'Another Region') ], count_diff: 1, args: [ 'Another Region'        ] }
+      { attributes_collection: [ model_attributes.merge(scheme_id: SecureRandom.uuid                        ) ], count_diff: 1, args: [ model_attributes[:name] ] },
+      { attributes_collection: [ model_attributes.merge(scheme_id: SecureRandom.uuid                        ) ], count_diff: 0, args: [ 'Another Region'        ] },
+      { attributes_collection: [ model_attributes.merge(scheme_id: SecureRandom.uuid, name: 'Another Region') ], count_diff: 0, args: [ model_attributes[:name] ] },
+      { attributes_collection: [ model_attributes.merge(scheme_id: SecureRandom.uuid, name: 'Another Region') ], count_diff: 1, args: [ 'Another Region'        ] }
     ]
 
     it_behaves_like 'scope', :being_virtual, [
-      { attributes_collection: [ model_attributes                      ], count_diff: 0, args: [       ] },
-      { attributes_collection: [ model_attributes                      ], count_diff: 0, args: [ true  ] },
-      { attributes_collection: [ model_attributes                      ], count_diff: 1, args: [ false ] },
-      { attributes_collection: [ model_attributes.merge(virtual: true) ], count_diff: 1, args: [       ] },
-      { attributes_collection: [ model_attributes.merge(virtual: true) ], count_diff: 1, args: [ true  ] },
-      { attributes_collection: [ model_attributes.merge(virtual: true) ], count_diff: 0, args: [ false ] }
+      { attributes_collection: [ model_attributes.merge(scheme_id: SecureRandom.uuid               ) ], count_diff: 0, args: [       ] },
+      { attributes_collection: [ model_attributes.merge(scheme_id: SecureRandom.uuid               ) ], count_diff: 0, args: [ true  ] },
+      { attributes_collection: [ model_attributes.merge(scheme_id: SecureRandom.uuid               ) ], count_diff: 1, args: [ false ] },
+      { attributes_collection: [ model_attributes.merge(scheme_id: SecureRandom.uuid, virtual: true) ], count_diff: 1, args: [       ] },
+      { attributes_collection: [ model_attributes.merge(scheme_id: SecureRandom.uuid, virtual: true) ], count_diff: 1, args: [ true  ] },
+      { attributes_collection: [ model_attributes.merge(scheme_id: SecureRandom.uuid, virtual: true) ], count_diff: 0, args: [ false ] }
     ]
 
   end
